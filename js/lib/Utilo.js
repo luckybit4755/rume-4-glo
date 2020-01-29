9336279322fccbbeb826abdef030e52cb43cb09e
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-01-29 09:40:10 -0500

    refactoring from https://github.com/luckybit4755/rando-calrissian/tree/master/html/demo/webgl

---
+++ js/lib/Utilo.js
@@ -0,0 +1,129 @@
+const Utilo = {
+	fullscreen: function( element ) {
+		let fz = 'webkitRequestFullscreen requestFullScreen mozRequestFullScreen msRequestFullscreen webkitRequestFullscreen webkitRequestFullscreen'.split( ' ' );
+
+		for ( let i = 0 ; i < fz.length ; i++ ) {
+			let f = fz[ i ];
+			if ( f in element ) {
+				element[ f ]();
+				break;
+			}
+		}
+	}
+	, frame: function( drawCallback, fps ) {
+		fps = fps || 24;
+
+		let frameFunction = function() {};
+		frameFunction.count = 0;
+		frameFunction.timeOut = false;
+
+		frameFunction.start = function() {
+			frameFunction.count++;
+			drawCallback();
+
+			frameFunction.timeOut = setTimeout(
+				function() {
+					requestAnimationFrame( frameFunction.start );
+				}
+				, 1000 / fps
+			);
+		};
+
+		frameFunction.stop = function() {
+			if ( frameFunction.timeOut ) {
+				clearTimeout( frameFunction.timeOut );
+				frameFunction.timeOut = false;
+			}
+		};
+
+		return frameFunction;
+	}
+	, idk: function( v, alternative ) {
+		return 'undefined' === typeof( v ) ? alternative : v;
+	}
+	, getByTag: function( tag, index ) {
+		tag = Utilo.idk( tag, 'canvas' );
+		index = Utilo.idk( index, 0 );
+		let tagged = Array.from( document.getElementsByTagName( tag ) );
+		return isNaN( index ) ? tagged : tagged[ index ];
+	}
+	, getById: function( id ) {
+		return document.getElementById( id );
+	}
+	, getByContents: function( value ) {
+		let treeWalker = document.createTreeWalker(
+			document.body
+			, NodeFilter.SHOW_TEXT
+			, { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT } }
+			, false
+		);
+
+		while( treeWalker.nextNode() ) {
+			let node = treeWalker.currentNode;
+			if ( value === node.nodeValue.trim() ) {
+				return node.parentNode;
+			}
+		}
+		return false;
+	}
+	, makeElement: function( stuff ) {
+		stuff.type = stuff.type || 'div';
+		let element = document.createElement( stuff.type );
+		for ( let k in stuff ) {
+			if ( /^(kids|type)$/.test( k ) ) continue;
+			switch ( k ) {
+				case 'txt': element.appendChild( document.createTextNode( stuff[ k ] ) ); break;
+				default: element.setAttribute( k, stuff[ k ] );
+			}
+		}
+		if ( 'kids' in stuff ) {
+			for ( let i = 0 ; i < stuff.kids.length ; i++ ) {
+				element.appendChild( makeElement( stuff.kids[ i ] ) );
+			}
+		}
+		return element;
+	}
+	, elementPosition: function( element ) {
+		/* from http://www.quirksmode.org/js/findpos.html */
+		let left = 0;
+		let top = 0;
+		if ( element.offsetParent ) {
+			do {
+				left += element.offsetLeft;
+				top  += element.offsetTop;
+			} while ( element = element.offsetParent );
+		}
+		return { x : left, y : top };
+	}
+	, identity: function( value ) {
+		return value;
+	}
+	, flatten: function( value, converter, values ) {
+		converter = Utilo.idk( converter, Utilo.identity );
+		values = Utilo.idk( values, [] );
+		if ( 'object' === typeof( value ) ) {
+			for ( let key in value ) {
+				Utilo.flatten( value[ key ], converter, values );
+			}
+		} else {
+			values.push( converter( value ) );
+		}
+		return values;
+	}
+	, floatless: function( v, precision ) {
+		if ( isNaN( v ) ) return v;
+		precision = Utilo.idk( precision, 1000 );
+		return Math.floor( v * precision ) / precision
+	}
+	, pushAll: function( current, nu ) {
+		for ( let i = 0 ; i < nu.length ; i++ ) { 
+			current.push( nu[ i ] );
+		}
+		return current;
+	}
+	, isString: function( s ) {
+		return 'string' == typeof( s ) || s instanceof String;
+	}
+};
+
+export default Utilo;
