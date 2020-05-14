b9409dfbdd0ddb7ec54dfabc25276b17aaa21e89
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-05-14 18:51:15 -0400

    sick of trying to fooling with js packaging

---
+++ bin/minnow.sh
@@ -0,0 +1,33 @@
+#!/bin/bash	
+
+_minnow_main() {
+	find src/lib -type f | xargs cat | _minnow_it
+}
+
+_minnow_it() {
+	_minnow_commies \
+	| _minnow_modless \
+	| _minnow_spaceless 
+}
+
+_minnow_commies() {
+	sed 's,/\*,☃&☃,g;s,\*/,☃&☃,' \
+	| tr '☃' '\n' \
+	| sed 's,//.*,,' \
+	| awk '/\/\*/ { K = 1 } !K { print } /\*\// { K = 0 } ' 
+}
+
+_minnow_modless() {
+	sed 's,^import .*,,;s,^export .*,,' 
+}
+
+
+_minnow_spaceless() {
+	tr '\t' ' ' | sed -E 's,^ +,,;s, +, ,g;s, +$,,' \
+	| sed 's, *\([[,{}()=!@<;>#?:+*/\-]\) *,\1,g;s, ],],g;' \
+	| grep -v '^$' \
+	| tr -d '\n'
+	echo
+}
+
+_minnow_main ${*}
