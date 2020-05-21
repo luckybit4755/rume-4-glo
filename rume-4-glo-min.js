0efca93d72a24963a70fa9d98b7786e735abd205
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-05-21 14:06:20 -0400

    added Vertexo

--- rume-4-glo-min.js
+++ rume-4-glo-min.js
@@ -1,3 +1,53 @@
+const Vertexo={
+surfaceNormal:function(normals,vertices,n,i,j,k,tmp1,tmp2){
+let v0=Utilo.idk(tmp1,new Float32Array(3));
+let v1=Utilo.idk(tmp2,new Float32Array(3));
+this.subtract(vertices,v0,i,j,0);
+this.subtract(vertices,v1,k,j,0);
+this.normalize(v0,v0,0,0);
+this.normalize(v1,v1,0,0);
+this.cross(v0,v1,normals,0,0,n);
+}
+,add:function(vertices,to,i,j,o){
+to[o+0]=vertices[i+0]+vertices[j+0];
+to[o+1]=vertices[i+1]+vertices[j+1];
+to[o+2]=vertices[i+2]+vertices[j+2];
+}
+,subtract:function(vertices,to,i,j,o){
+to[o+0]=vertices[i+0]-vertices[j+0];
+to[o+1]=vertices[i+1]-vertices[j+1];
+to[o+2]=vertices[i+2]-vertices[j+2];
+}
+,normalize:function(vertices,to,i,o){
+let length=this.length(vertices,i);
+if(0==length)length=1;
+to[o+0]=vertices[i+0]/length;
+to[o+1]=vertices[i+1]/length;
+to[o+2]=vertices[i+2]/length;
+}
+,length:function(vertices,i){
+return Math.sqrt(this.length2(vertices,i));
+}
+,length2:function(vertices,i){
+return(
+vertices[i+0]*vertices[i+0]
++vertices[i+1]*vertices[i+1]
++vertices[i+2]*vertices[i+2]
+);
+}
+,cross:function(v0,v1,normals,i,j,n){
+normals[n+0]=v0[i+1]*v1[j+2]-v0[i+2]*v1[j+1]
+normals[n+1]=v0[i+2]*v1[j+0]-v0[i+0]*v1[j+2]
+normals[n+2]=v0[i+0]*v1[j+1]-v0[i+1]*v1[j+0]
+}
+,dot:function(v0,v1,i,j){
+return(
+v0[i+0]*v1[j+0]
++v0[i+1]*v1[j+1]
++v0[i+2]*v1[j+2]
+)
+}
+};
 const Vectoro={
 set:function(x,y,z){
 return[x,y,z];
@@ -607,6 +657,7 @@ Glo.data(gl,program,name,data);
 Glo.draw(gl,mesh.faces,type);
 }
 ,demoSetup:function(shaders,matrixName,flags){
+shaders=Utilo.idk(shaders,Shadero.lit);
 matrixName=Utilo.idk(matrixName,'uMatrix');
 flags=Utilo.idk(flags,{});
 let canvas=Utilo.getByTag('canvas');
